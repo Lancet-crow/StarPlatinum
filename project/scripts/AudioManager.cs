8032a1eb7d947517a1e1457c007c4b4c2be946ad
Alex-Degtyarenko <aleksdegtyarenko20@gmail.com>
2024-12-03 23:16:17 +0400

    Добавил аудиосистему
    Добавил аудиосистему и плагин Serialized Dictionary(гораздо легче работать со словарями в инспекторах, чем без них)

---
+++ project/scripts/AudioManager.cs
@@ -0,0 +1,93 @@
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic((int)environment);
+    }
+
+    private void OnDisable()
+    {
+        AudioMusic.Stop();
+        AudioSounds.Stop();
+        AudioMusic.clip = null;
+        AudioSounds.clip = null;
+    }
+
+    public enum SoundType
+    {
+        breakBuilding,
+        buildBuilding,
+        closeMenu,
+        openMenu
+    }
+
+    public enum MusicType
+    {
+        mainMenuMusic,
+        ingameMusic
+    }
+
+
+    [SerializedDictionary]
+    public SerializedDictionary<SoundType, AudioClip> soundClips = new();
+    [SerializedDictionary]
+    public SerializedDictionary<MusicType, AudioClip> musicClips = new();
+
+    public AudioSource AudioSounds;
+    public AudioSource AudioMusic;
+
+    public enum Environment
+    {
+        mainMenu,
+        inGame
+    }
+
+    [SerializeField]
+    private Environment environment;
+
+    public void PlaySound(SoundType soundType)
+    {
+        AudioSounds.PlayOneShot(soundClips[soundType]);
+    }
+
+    public void PlaySound(int soundTypeInt)
+    {
+        AudioSounds.PlayOneShot(soundClips[(SoundType)soundTypeInt]);
+    }
+
+    public void PlaySound(string soundTypeName)
+    {
+        AudioSounds.PlayOneShot(soundClips[(SoundType)System.Enum.Parse(typeof(SoundType), soundTypeName)]);
+    }
+
+    public void PlayMusic(MusicType musicType)
+    {
+        AudioMusic.PlayOneShot(musicClips[musicType]);
+    }
+
+    public void PlayMusic(int musicTypeInt)
+    {
+        AudioMusic.PlayOneShot(musicClips[(MusicType)musicTypeInt]);
+    }
+
+    public void PlayMusic(string musicTypeName)
+    {
+        AudioMusic.PlayOneShot(musicClips[(MusicType)System.Enum.Parse(typeof(MusicType), musicTypeName)]);
+    }
+}
